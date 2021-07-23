a9921e00bebc375cde3490463eb08e25208b2350
Ludvig Hagmar <ludvig.hagmar@bombayworks.se>
2021-07-23 10:35:05 +0200

    Desktopmenu

---
+++ AEKWeb/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AEKWeb.Controllers
+{
+
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Calendar")]
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        [Route("Files")]
+        public IActionResult Files()
+        {
+            return View();
+        }
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
--- AEKWeb/Startup.cs
+++ AEKWeb/Startup.cs
@@ -1,17 +1,11 @@
 using AEKWeb.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AEKWeb
 {
@@ -59,10 +53,11 @@ namespace AEKWeb
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                    "StartPage",
+                    "",
+                    new { controller = "Page", action = "Index" });
             });
         }
     }
